e3998f9a18dc468d989fabdda16a06a5a851c60f
Yuki Sato <yuki.sato.dev@gmail.com>
2019-01-23 13:09:38 -0800

    Init 136

---
+++ leetcode/136.single-number/solution.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ */
+function singleNumber($nums) {
+    return $nums;
+}
+
+var_dump(singleNumber([2,2,1]));
+var_dump(singleNumber([4,1,2,1,2]));

a86b25d74dff3e5a36878d9accb69ecb326c3241
Yuki Sato <yuki.sato.dev@gmail.com>
2019-01-23 13:10:55 -0800

    solve 344

--- leetcode/344.reverse-string/solution.php
+++ leetcode/344.reverse-string/solution.php
@@ -1,6 +1,28 @@
 <?php
-function reverseString($s) {
-    return $s;
+function reverseString1(&$s) {
+    $len = count($s);
+    foreach ($s as $i => $char) {
+        $s[$len - $i] = $char;
+    }
+    unset($s[0]);
 }
-var_dump(reverseString(['h','e','l','l','o']));
-var_dump(reverseString(['H','a','n','n','a','h']));
+// 168ms
+function reverseString(&$s) {
+    $len = count($s);
+    $max = floor($len/2);
+    $len--;
+    for ($i = 0; $i < $max; $i++) {
+        $j = $len - $i;
+        $tmp = $s[$i];
+        $s[$i] = $s[$j];
+        $s[$j] = $tmp;
+    }
+}
+$input1 = ['h','e','l','l','o'];
+reverseString($input1);
+var_dump($input1);
+
+$input2 = ['H','a','n','n','a','h'];
+reverseString($input2);
+var_dump($input2);
+

a51b3cf30af95ae8810ebb84ceae914226003b73
Yuki Sato <yuki.sato.dev@gmail.com>
2019-01-25 15:25:02 -0800

    init 884

---
+++ leetcode/884.uncommon-words-from-two-sentences/solutions.php
@@ -0,0 +1,10 @@
+<?php
+function uncommonFromSentences($A, $B) {
+
+}
+
+echo uncommonFromSentences("this apple is sweet", "this apple is sour")."\n";
+// Output: ["sweet","sour"]
+
+echo uncommonFromSentences("apple apple", "banana")."\n";
+// Output: ["banana"]

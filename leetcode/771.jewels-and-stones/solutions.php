e25494441bdd0fb313c34620aa9b573cfe2d2782
Yuki Sato <yuki.sato.dev@gmail.com>
2019-01-25 15:08:51 -0800

    init 771

---
+++ leetcode/771.jewels-and-stones/solutions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+ *
+ * The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ */
+function numJewelsInStones($J, $S) {
+
+}
+
+echo numJewelsInStones("aA", "aAAbbbb")."\n"; // should return 2
+echo numJewelsInStones("z", "ZZ")."\n"; // should return 0

67dd8fdf153c4d43468c8ec66e565150de9d2bc3
Yuki Sato <yuki.sato.dev@gmail.com>
2019-01-28 17:02:45 -0800

    solve 965

--- leetcode/965.univalued-binary-tree/solution.php
+++ leetcode/965.univalued-binary-tree/solution.php
@@ -4,8 +4,39 @@
  *
  * Return true if and only if the given tree is univalued.
  */
-function isUnivalTree($root) {
 
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+    public function isUnivalTree($root) {
+        $res = self::compare($root);
+        return ($res !== false);
+    }
+
+    public static function compare($node) {
+        $left = $right = null;
+        if (isset($node->left)) $left = self::compare($node->left);
+        if (isset($node->right)) $right = self::compare($node->right);
+        if ($left === false || $right === false) return false;
+
+        if ($left === null) {
+            if ($right === null) return $node->val;
+            if ($right === $node->val) return $node->val;
+            return false;
+        } elseif ($right === null) {
+            if ($left === $node->val) return $node->val;
+            return false;
+        } elseif ($right === $left && $left === $node->val) {
+            return $node->val;
+        } else {
+            return false;
+        }
+    }
 }
-var_dump(isUnivalTree([1,1,1,1,1,null,1]); // should be true
-var_dump(isUnivalTree([2,2,2,5,2]); // should be false

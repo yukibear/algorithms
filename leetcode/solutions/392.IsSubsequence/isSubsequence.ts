a7a04ddd6702b78d78e657e1d91512fa022aa7d7
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-09 15:40:01 -0800

    improve #392 using charCodeAt()

--- leetcode/solutions/392.IsSubsequence/isSubsequence.ts
+++ leetcode/solutions/392.IsSubsequence/isSubsequence.ts
@@ -11,8 +11,8 @@ export default function isSubsequence(
   if (subsequence.length === 0) return true;
 
   let i = 0;
-  for (const s of target) {
-    if (subsequence[i] === s) {
+  for (let j = 0; j < target.length; j++) {
+    if (subsequence.charCodeAt(i) === target.charCodeAt(j)) {
       i++;
       if (i === subsequence.length) return true;
     }

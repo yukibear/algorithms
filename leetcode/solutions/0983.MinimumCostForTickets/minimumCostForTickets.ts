7b32374900199720a903925d31116faf5acf19a3
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-09 15:45:04 -0800

    init #0983

---
+++ leetcode/solutions/0983.MinimumCostForTickets/minimumCostForTickets.ts
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} days
+ * @param {number[]} costs
+ * @return {number}
+ */
+export default function minimumCostForTickets(
+  days: number[],
+  costs: number[]
+): number {
+
+}
---
+++ leetcode/solutions/0983.MinimumCostForTickets/minimumCostForTickets_test.ts
@@ -0,0 +1,8 @@
+import { test } from 'https://deno.land/std/testing/mod.ts';
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import minimumCostForTickets from './minimumCostForTickets.ts';
+
+test('0983. Minimum Cost For Tickets', () => {
+  assertEquals(minimumCostForTickets([1,4,6,7,8,20], [2,7,15]), 11);
+  assertEquals(minimumCostForTickets([1,2,3,4,5,6,7,8,9,10,30,31], [2,7,15]), 17);
+});

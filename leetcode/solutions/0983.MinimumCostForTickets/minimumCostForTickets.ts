23e03a9c244fa6682c3f1543e19034625e9edfcb
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-09 17:52:24 -0800

    #0983 done

--- leetcode/solutions/0983.MinimumCostForTickets/minimumCostForTickets.ts
+++ leetcode/solutions/0983.MinimumCostForTickets/minimumCostForTickets.ts
@@ -5,7 +5,23 @@
  */
 export default function mincostTickets(
   days: number[],
-  costs: number[]
+  costs: [number, number, number]
 ): number {
+  const [c1, c7, c30] = [...costs];
 
+  let daySet = new Set([...days]);
+  let dp = new Array(days.length + 30).fill(0);
+  const max = days[days.length - 1] + 30;
+  let j = 0;
+  for (let i = 31; i <= max; i++) {
+    j++;
+    if (!daySet.has(i - 30)) {
+      dp[i] = dp[i - 1];
+      continue;
+    }
+
+    dp[i] = Math.min(dp[i-1] + c1, dp[i-7] + c7, dp[i-30] + c30);
+  }
+
+  return dp[dp.length - 1];
 }

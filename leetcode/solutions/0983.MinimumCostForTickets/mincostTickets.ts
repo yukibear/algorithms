67c892eced9fd561caab7814eaf11b89c42bb55f
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-11 08:42:22 -0800

    #0983 remove an unused variable

--- leetcode/solutions/0983.MinimumCostForTickets/mincostTickets.ts
+++ leetcode/solutions/0983.MinimumCostForTickets/mincostTickets.ts
@@ -12,9 +12,7 @@ export default function mincostTickets(
   let daySet = new Set([...days]);
   let dp = new Array(days.length + 30).fill(0);
   const max = days[days.length - 1] + 30;
-  let j = 0;
   for (let i = 31; i <= max; i++) {
-    j++;
     if (!daySet.has(i - 30)) {
       dp[i] = dp[i - 1];
       continue;

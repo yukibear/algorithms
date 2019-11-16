c7fef345eecf6d892c6afa9fe6036f4a85b3f1ed
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-15 17:03:14 -0800

    init 53. Maximum Subarray

---
+++ leetcode/solutions/0053.MaximumSubarray/maximumSubarray.ts
@@ -0,0 +1,9 @@
+export default function maximumSubarray(
+  str: string,
+  num: number
+): boolean {
+
+  // write some code here
+
+  return false;
+}
---
+++ leetcode/solutions/0053.MaximumSubarray/maximumSubarray_test.ts
@@ -0,0 +1,8 @@
+import { test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import maximumSubarray from "./maximumSubarray.ts";
+
+test("0053. Maximum Subarray", () => {
+  const testValue = "";
+  assertEquals(maximumSubarray(testValue, 777), true);
+});

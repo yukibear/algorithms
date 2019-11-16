eb9f02e23a6ae472aee82d476a490591173d3935
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-15 17:44:05 -0800

    #0053 solve

---
+++ leetcode/solutions/0053.MaximumSubarray/maxSubArray.ts
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+export default function maxSubArray(
+  nums: number[],
+): number {
+
+  let max = nums[0];
+  let prevMax = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    prevMax = Math.max(prevMax + nums[i], nums[i]);
+    if (prevMax > max) max = prevMax;
+  }
+
+  return max;
+}
--- leetcode/solutions/0053.MaximumSubarray/maxSubArray_test.ts
+++ leetcode/solutions/0053.MaximumSubarray/maxSubArray_test.ts
@@ -1,7 +1,7 @@
 import { test } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import maximumSubarray from "./maximumSubarray.ts";
+import maxSubArray from "./maxSubArray.ts";
 
 test("0053. Maximum Subarray", () => {
-  assertEquals(maximumSubarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
+  assertEquals(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
 });

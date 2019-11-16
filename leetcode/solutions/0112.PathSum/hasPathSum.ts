e9cb7e75f3d53e24a9e2f154f0ba402b7f6cf61a
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-16 15:47:32 -0800

    #0112 refactor

--- leetcode/solutions/0112.PathSum/hasPathSum.ts
+++ leetcode/solutions/0112.PathSum/hasPathSum.ts
@@ -9,28 +9,14 @@ export interface TreeNode {
  * @param {number} sum
  * @return {boolean}
  */
-export default function hasPathSum(
-  root: TreeNode,
-  sum: number
-): boolean {
-  if (!root) return false;
+export default function hasPathSum(node: TreeNode, sum: number): boolean {
+  if (!node) return false;
 
-  const dfs = (node: TreeNode, num: number): boolean => {
-    if (!node.left && !node.right) {
-      if (num === 0) return true;
-      return false;
-    }
+  sum -= node.val;
 
-    if (node.left) {
-      let leftSum = num - node.left.val;
-      if (dfs(node.left, leftSum) === true) return true
-    }
-    if (node.right) {
-      let rightSum = num - node.right.val;
-      return dfs(node.right, rightSum);
-    }
-    return false;
+  if (!node.left && !node.right) {
+    return sum === 0;
   }
 
-  return dfs(root, sum - root.val);
+  return hasPathSum(node.left!, sum) || hasPathSum(node.right!, sum);
 }

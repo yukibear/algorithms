96da0827e3fe72b4c09f7c0d296d4a891f328d1e
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-17 03:33:38 -0800

    #0198 resolve

--- leetcode/solutions/0198.HouseRobber/rob.ts
+++ leetcode/solutions/0198.HouseRobber/rob.ts
@@ -6,6 +6,12 @@ export default function rob(
   nums: number[]
 ): number {
 
+  let dp = new Array(nums.length + 2);
+  dp[0] = 0;
+  dp[1] = 0;
 
-  return 0;
+  for (let i = 2; i < nums.length+2; i++) {
+    dp[i] = Math.max((dp[i-2] + nums[i-2]), dp[i-1]);
+  }
+  return (dp[dp.length-2] > dp[dp.length-1]) ? dp[dp.length-2] : dp[dp.length-1];
 }

e8ca0fc89594e5f325cdda494efc11a5ac2511b7
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-20 10:19:04 -0800

    #0141 improve in speed

--- leetcode/solutions/0141.LinkedListCycle/hasCycle.ts
+++ leetcode/solutions/0141.LinkedListCycle/hasCycle.ts
@@ -1,21 +1,18 @@
 import { SinglyLinkedListNode } from "../../../data_structures/SinglyLinkedListNode.ts";
+
 /**
- * @param {ListNode} head
+ * @param {SinglyLinkedListNode} head
  * @return {boolean}
  */
 export default function hasCycle(
   node: SinglyLinkedListNode<number> | null,
 ): boolean {
-  if (node == null) return false;
-
   const nodeSet = new Set();
 
-  while (node.next) {
-    node = node.next;
-
-    if (nodeSet.has(node)) return true;
-
+  while (node) {
     nodeSet.add(node);
+    node = node.next;
+    if (nodeSet.has(node)) return true;
   }
 
   return false;

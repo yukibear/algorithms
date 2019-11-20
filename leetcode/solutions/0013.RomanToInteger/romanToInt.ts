95184de41d1fcd3d627b627638e470c113c6dab4
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-20 12:19:23 -0800

    #0013 resolve

--- leetcode/solutions/0013.RomanToInteger/romanToInt.ts
+++ leetcode/solutions/0013.RomanToInteger/romanToInt.ts
@@ -1,20 +1,28 @@
 /**
- * Symbol       Value
- * I             1
- * V             5
- * X             10
- * L             50
- * C             100
- * D             500
- * M             1000
- *
  * @param {string} s
  * @return {number}
  */
+const romanNums = new Map([
+  ["I", 1],
+  ["V", 5],
+  ["X", 10],
+  ["L", 50],
+  ["C", 100],
+  ["D", 500],
+  ["M", 1000],
+]);
+
 export default function romanToInt(
   s: string
 ): number {
-  // assertEquals(romanToInt("LVIII"), 58);
-  // assertEquals(romanToInt("MCMXCIV"), 1994);
-  return 0;
+  let sum = 0;
+  let prevNum = 0;
+
+  for (let i = s.length - 1; i >= 0; --i) {
+    let num = romanNums.get(s.charAt(i)) || 0;
+    sum += (num < prevNum) ? -num : num;
+    prevNum = num;
+  }
+
+  return sum;
 }

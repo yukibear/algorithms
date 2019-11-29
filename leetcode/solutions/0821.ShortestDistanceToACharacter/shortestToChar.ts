833b875d4a641a4a956e29cbdf020f59a62fb4bb
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-29 12:22:31 -0800

    #0821 refactor code

--- leetcode/solutions/0821.ShortestDistanceToACharacter/shortestToChar.ts
+++ leetcode/solutions/0821.ShortestDistanceToACharacter/shortestToChar.ts
@@ -1,19 +1,21 @@
 /**
  * @param {string} S
- * @param {character} C
+ * @param {string} C
  * @return {number[]}
  */
 export default function shortestToChar(
   str: string,
   char: string
 ): number[] {
-  let rightIndex;
+  let rightIndex = Infinity;
   let leftIndex = -Infinity;
   let result = new Array(str.length);
 
   for (let i = 0; i < str.length; i++) {
-    rightIndex = str.indexOf(char, i);
-    result[i] = rightIndex !== -1 ? Math.min(i - leftIndex, rightIndex - i) : i - leftIndex
+    if ((rightIndex = str.indexOf(char, i)) === -1) rightIndex = Infinity;
+
+    result[i] = Math.min(i - leftIndex, rightIndex - i);
+
     if (rightIndex === i) leftIndex = rightIndex;
   }
 

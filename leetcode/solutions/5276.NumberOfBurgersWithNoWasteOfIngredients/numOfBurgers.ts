8f2300855339d4944f3aa13812423717c8c38d4e
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-30 19:15:11 -0800

    #5276 init

---
+++ leetcode/solutions/5276.NumberOfBurgersWithNoWasteOfIngredients/numOfBurgers.ts
@@ -0,0 +1,12 @@
+/**
+ * @param {number} tomatoSlices
+ * @param {number} cheeseSlices
+ * @return {number[]}
+ */
+export default function numOfBurgers(
+  tomatoSlices: number,
+  cheeseSlices: number
+): number[] {
+
+  return [];
+}

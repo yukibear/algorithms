722c9797f0e7e2a4f91454665cca3570f89a2fcb
Yuki Sato <yuki.sato.dev@gmail.com>
2019-11-30 20:21:12 -0800

    #1276 rename from contest #5276

--- leetcode/solutions/1276.NumberOfBurgersWithNoWasteOfIngredients/numOfBurgers_test.ts
+++ leetcode/solutions/1276.NumberOfBurgersWithNoWasteOfIngredients/numOfBurgers_test.ts
@@ -2,7 +2,7 @@ import { test } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import numOfBurgers from "./numOfBurgers.ts";
 
-test("5276. Number of Burgers with No Waste of Ingredients", () => {
+test("1276. Number of Burgers with No Waste of Ingredients", () => {
   assertEquals(numOfBurgers(16, 7), [1,6]);
   assertEquals(numOfBurgers(17, 4), []);
   assertEquals(numOfBurgers(4, 17), []);

a8bf1aae64005568b5b1a7830d485264e8a5b61c
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-04 10:01:12 -0800

    WIP

--- leetcode/solutions/0669.TrimABinarySearchTree/trimBST.ts
+++ leetcode/solutions/0669.TrimABinarySearchTree/trimBST.ts
@@ -7,10 +7,31 @@ import { TreeNode } from "../../../data_structures/TreeNode.ts";
  * @return {TreeNode}
  */
 export default function trimBST(
-  node: TreeNode<number>,
+  node: TreeNode<number> | null,
   L: number,
   R: number
 ): TreeNode<number> | null {
+  const hasLeaf = (node: TreeNode<number> | null): boolean => {
+    if (!node) return false;
+    if (node.val === L) return true;
+
+    return hasLeaf(node.left) || hasLeaf(node.right);
+  }
+
+  const createAnswer = (node: TreeNode<number>) => {
+  }
+
+  let queueR: (TreeNode<number> | null)[] = [node];
+  while (queueR.length > 0) {
+    let node: TreeNode<number> | null | undefined= queueR.pop();
+    if (!node) continue;
+    queueR.push(node.left);
+    queueR.push(node.right);
+
+    if (node.val === R) {
+      if (hasLeaf(node)) return createAnswer(node);
+    }
+  }
 
   return null;
 }

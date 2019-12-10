fb5f894c3e1bee1d693a4d2933babc71a9ab71db
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-10 15:04:00 -0800

    #0859 refactor code

--- leetcode/solutions/0859.BuddyStrings/buddyStrings.ts
+++ leetcode/solutions/0859.BuddyStrings/buddyStrings.ts
@@ -3,29 +3,18 @@
  * @param {string} B
  * @return {boolean}
  */
-export default function buddyStrings(
-  A: string,
-  B: string
-): boolean {
-  if (A.length !== B.length) return false;
+export default function buddyStrings(A: string, B: string): boolean {
+  if (A.length !== B.length) {
+    return false;
+  }
 
-  const map = new Map<string, number>();
-  let diffs: number[] = [];
+  let diff: number[] = [];
   for (let i = 0; i < A.length; i++) {
-    map.set(A[i], (map.get(A[i]) || 0) + 1);
     if (A[i] !== B[i]) {
-      if (diffs.length === 2) return false;
-      diffs.push(i);
+      diff.push(i);
     }
   }
 
-  if (diffs.length === 1) return false;
-  if (diffs.length === 2) {
-    if (A[diffs[0]] === B[diffs[1]] && A[diffs[1]] === B[diffs[0]]) {
-      return true;
-    } else {
-      return false;
-    }
-   }
-  return Array.from(map.values()).filter(n => n > 1).length > 0;
+  return diff.length === 2 && A[diff[0]] === B[diff[1]] && A[diff[1]] === B[diff[0]]
+    || diff.length === 0 && new Set<string>(A).size !== A.length;
 }

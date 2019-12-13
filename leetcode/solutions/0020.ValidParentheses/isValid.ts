e46181d80ce5f24714976287844c9bab50faf00b
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-13 15:10:51 -0800

    #0020 resolve

---
+++ leetcode/solutions/0020.ValidParentheses/isValid.ts
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+export default function isValid(
+  s: string
+): boolean {
+  if (s.length % 2 !== 0) {
+    return false;
+  }
+
+  const closerMap = new Map([["{", "}"], ["(", ")"], ["[", "]"]]);
+  const openerSet = new Set(closerMap.keys());
+  const closerStack: string[] = [];
+  for (let i = 0; i < s.length; i++) {
+    if (openerSet.has(s[i])) {
+      closerStack.push(closerMap.get(s[i])!);
+    } else if (closerStack.pop() !== s[i]) {
+      return false;
+    }
+  }
+  return closerStack.length === 0;
+}

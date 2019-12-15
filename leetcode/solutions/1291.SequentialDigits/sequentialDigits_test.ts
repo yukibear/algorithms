33751e7af14ad8887514eff5c63ed653ef1d186e
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-14 22:07:07 -0800

    rename from contest

--- leetcode/solutions/1291.SequentialDigits/sequentialDigits_test.ts
+++ leetcode/solutions/1291.SequentialDigits/sequentialDigits_test.ts
@@ -2,7 +2,7 @@ import { test } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import sequentialDigits from "./sequentialDigits.ts";
 
-test("5124. Sequential Digits", () => {
+test("1291. Sequential Digits", () => {
   assertEquals(sequentialDigits(100, 300), [123,234]);
   assertEquals(sequentialDigits(1000, 13000), [1234,2345,3456,4567,5678,6789,12345]);
 });

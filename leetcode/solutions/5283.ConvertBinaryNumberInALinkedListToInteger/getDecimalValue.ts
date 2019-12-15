e9adc60e66bc05afcac516b4116c7ff032ab42c8
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-14 18:33:52 -0800

    #5283 init

---
+++ leetcode/solutions/5283.ConvertBinaryNumberInALinkedListToInteger/getDecimalValue.ts
@@ -0,0 +1,8 @@
+import { SinglyLinkedListNode as ListNode } from "../../../data_structures/SinglyLinkedListNode.ts";
+
+export default function getDecimalValue(
+  head: ListNode<number> | null
+): number {
+
+  return 0;
+}

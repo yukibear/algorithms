eb099343613aaa0d237e5e9321e06d6ac4df4167
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-14 16:21:24 -0800

    #0700 refactor code

--- leetcode/solutions/0700.SearchInABinarySearchTree/searchBST_test.ts
+++ leetcode/solutions/0700.SearchInABinarySearchTree/searchBST_test.ts
@@ -1,10 +1,10 @@
 import { test } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import searchBST from "./searchBST.ts";
-import { TreeNode } from "../../../data_structures/TreeNode.ts";
+import { TreeNode as BST } from "../../../data_structures/TreeNode.ts";
 
 test("0700. Search in a Binary Search Tree", () => {
-  let tree: TreeNode<number> = {
+  let tree: BST<number> = {
     val: 4,
     left: {
       val: 2,
@@ -13,7 +13,7 @@ test("0700. Search in a Binary Search Tree", () => {
     },
     right: { val: 7, left: null, right: null },
   };
-  let expected: TreeNode<number> = {
+  let expected: BST<number> = {
     val: 2,
     left: { val: 1, left: null, right: null },
     right: { val: 3, left: null, right: null },

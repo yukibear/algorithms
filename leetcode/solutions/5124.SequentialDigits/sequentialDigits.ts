50207d5fc85a8c2ccfeec04c40051a5ff478a172
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-14 19:19:55 -0800

    #5124 resolve

--- leetcode/solutions/5124.SequentialDigits/sequentialDigits.ts
+++ leetcode/solutions/5124.SequentialDigits/sequentialDigits.ts
@@ -2,6 +2,27 @@ export default function sequentialDigits(
   low: number,
   high: number
 ): number[] {
+  let result: number[] = [];
 
-  return [0];
+  let lowKeta = low.toString().length;
+  let highKeta = high.toString().length;
+
+  loop:
+  for (let keta = lowKeta; keta <= highKeta; keta++) {
+    for (let i = 1; i <= 10 - keta; i++) {
+      let num = 0;
+      for (let j = keta-1, n = i; j >= 0 && n < 10; j--, n++) {
+        num += 10 ** j * n;
+        if (num === 7890) break loop;
+      }
+      if (num > high) {
+        break loop;
+      }
+      if (num >= low && num <= high) {
+        result.push(num);
+      }
+    }
+  }
+
+  return result;
 }

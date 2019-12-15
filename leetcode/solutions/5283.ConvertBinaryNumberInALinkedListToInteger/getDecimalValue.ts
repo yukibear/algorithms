5b2538414cd2d22a4968db267e0cffeb9c03c1e1
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-14 18:39:50 -0800

    #5283 resolve

--- leetcode/solutions/5283.ConvertBinaryNumberInALinkedListToInteger/getDecimalValue.ts
+++ leetcode/solutions/5283.ConvertBinaryNumberInALinkedListToInteger/getDecimalValue.ts
@@ -4,5 +4,18 @@ export default function getDecimalValue(
   head: ListNode<number> | null
 ): number {
 
-  return 0;
+  const nums: number[] = [];
+  while (head) {
+    nums.unshift(head.val);
+    head = head.next;
+  }
+
+  let sum = 0;
+  let x = 1;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i] * x;
+    x *= 2;
+  }
+
+  return sum;
 }

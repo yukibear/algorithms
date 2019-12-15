06af7dbfe76d35ca29a527f6c120e7fcf595cc72
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-14 19:58:10 -0800

    #5286 init

---
+++ leetcode/solutions/5286.ShortestPathInAGridWithObstaclesElimination/shortestPath.ts
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number}
+ */
+export default function shortestPath(
+  grid: number[][],
+  k: number
+): number {
+
+  return 0;
+}

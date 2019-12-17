cd2be3aebc28cc91a4f634f416beba49aa78196d
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-16 21:11:59 -0800

    #0720 init

---
+++ leetcode/solutions/0720.LongestWordInDictionary/longestWord.ts
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+export default function longestWord(words: string[]): string {
+
+  return "";
+}
---
+++ leetcode/solutions/0720.LongestWordInDictionary/longestWord_test.ts
@@ -0,0 +1,8 @@
+import { test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import longestWord from "./longestWord.ts";
+
+test("0720. Longest Word in Dictionary", () => {
+  assertEquals(longestWord(["w","wo","wor","worl", "world"]), "world");
+  assertEquals(longestWord(["a", "banana", "app", "appl", "ap", "apply", "apple"]), "apple");
+});

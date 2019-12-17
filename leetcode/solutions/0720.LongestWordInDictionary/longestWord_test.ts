4bd98ecc71ad80023adced6fd6f3784071e83542
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-16 22:14:29 -0800

    #0720 resolve

--- leetcode/solutions/0720.LongestWordInDictionary/longestWord_test.ts
+++ leetcode/solutions/0720.LongestWordInDictionary/longestWord_test.ts
@@ -5,4 +5,5 @@ import longestWord from "./longestWord.ts";
 test("0720. Longest Word in Dictionary", () => {
   assertEquals(longestWord(["w","wo","wor","worl", "world"]), "world");
   assertEquals(longestWord(["a", "banana", "app", "appl", "ap", "apply", "apple"]), "apple");
+  assertEquals(longestWord(["vsw","vs","zwu","vsx","nc","o","vswus","orv","imf","i","v","zw","vs"]), "vsw");
 });

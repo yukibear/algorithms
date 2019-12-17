e62f1532ded08a7f2fbe42565565a52118a5b695
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-16 20:43:51 -0800

    #0701 init

---
+++ leetcode/solutions/0701.InsertIntoABinarySearchTree/insertIntoBST.ts
@@ -0,0 +1,9 @@
+import { TreeNode } from "../../../data_structures/TreeNode.ts";
+
+export default function insertIntoBST(
+  root: TreeNode<number> | null,
+  val: number
+): TreeNode<number> | null {
+
+  return null;
+}

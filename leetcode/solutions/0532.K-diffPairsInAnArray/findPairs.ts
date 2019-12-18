2fc1134fe20da147ba3f288dda902788d0c29ec6
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-18 12:23:06 -0800

    #0532 resolve

---
+++ leetcode/solutions/0532.K-diffPairsInAnArray/findPairs.ts
@@ -0,0 +1,28 @@
+export default function findPairs(
+  nums: number[],
+  k: number
+): number {
+  if (k < 0) {
+    return 0;
+  }
+
+  const numMap = new Map<number, number>();
+
+  for (const n of nums) {
+    numMap.set(n, (numMap.get(n) || 0) + 1);
+  }
+
+  let numberOfPairs = 0;
+
+  for (const num of numMap.keys()) {
+    const pair = num + k;
+
+    if (!numMap.has(pair) || num === pair && numMap.get(pair) < 2) {
+      continue;
+    }
+
+    numberOfPairs++;
+  }
+
+  return numberOfPairs;
+}

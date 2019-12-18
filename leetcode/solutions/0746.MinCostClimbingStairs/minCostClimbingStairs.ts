fcc3ecea73af39438d6baa9e40acccdbb49babc1
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-18 12:24:08 -0800

    #0746 resolve

---
+++ leetcode/solutions/0746.MinCostClimbingStairs/minCostClimbingStairs.ts
@@ -0,0 +1,7 @@
+export default function minCostClimbingStairs(cost: number[]): number {
+  for (let i = 2; i < cost.length; i++) {
+    cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+  }
+
+  return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
+}

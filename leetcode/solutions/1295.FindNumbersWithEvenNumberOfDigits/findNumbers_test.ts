aed761f9388a15a4c94aa98157264c016c3c6798
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-22 03:52:37 -0800

    rename files from contest

--- leetcode/solutions/1295.FindNumbersWithEvenNumberOfDigits/findNumbers_test.ts
+++ leetcode/solutions/1295.FindNumbersWithEvenNumberOfDigits/findNumbers_test.ts
@@ -2,6 +2,6 @@ import { test } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import findNumbers from "./findNumbers.ts";
 
-test("5291. Find Numbers with Even Number of Digits", () => {
+test("1295. Find Numbers with Even Number of Digits", () => {
   assertEquals(findNumbers([12,345,2,6,7896]), 2);
 });

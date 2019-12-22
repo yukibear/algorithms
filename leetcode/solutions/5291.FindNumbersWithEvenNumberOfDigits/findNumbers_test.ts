2c5cdd7d87349729df0984fac176f52eb69e4a5c
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-22 03:47:48 -0800

    solve #5291 - #5294 from contest

---
+++ leetcode/solutions/5291.FindNumbersWithEvenNumberOfDigits/findNumbers_test.ts
@@ -0,0 +1,7 @@
+import { test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import findNumbers from "./findNumbers.ts";
+
+test("5291. Find Numbers with Even Number of Digits", () => {
+  assertEquals(findNumbers([12,345,2,6,7896]), 2);
+});

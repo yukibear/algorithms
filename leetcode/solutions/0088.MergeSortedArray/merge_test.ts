d1890f826e5fb23b6c648098b612126d8b699402
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-24 15:07:17 -0800

    #0088 solve

---
+++ leetcode/solutions/0088.MergeSortedArray/merge_test.ts
@@ -0,0 +1,13 @@
+import { test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import merge from "./merge.ts";
+
+test("0088. Merge Sorted Array", () => {
+  const arr1 = [1, 2, 3, 0, 0, 0];
+  merge(arr1, 3, [2, 5, 6], 3);
+  assertEquals(arr1, [1, 2, 2, 3, 5, 6]);
+
+  const arr2 = [-1, 0, 0, 3, 3, 3, 0, 0, 0];
+  merge(arr2, 6, [1, 2, 2], 3);
+  assertEquals(arr2, [-1, 0, 0, 1, 2, 2, 3, 3, 3]);
+});

0820b8d2b8c7079e3dc9f5aca8f4e92ac8f9efd2
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-23 18:25:03 -0800

    #0653 solve the problem with DFS

--- leetcode/solutions/0653.TwoSumIV-InputIsABST/findTarget.ts
+++ leetcode/solutions/0653.TwoSumIV-InputIsABST/findTarget.ts
@@ -1,9 +1,23 @@
 import { BinaryTreeNode } from "../../../data_structures/BinaryTreeNode.ts";
 
 export default function findTarget(
-  root: BinaryTreeNode<number> | null,
+  root: BinaryTreeNode<number>,
   k: number
 ): boolean {
+  const set = new Set<number>();
 
-  return false;
+  function dfs(node: BinaryTreeNode<number> | null): boolean {
+    if (!node) {
+      return false;
+    }
+    if (set.has(k - node.val)) {
+      return true;
+    }
+
+    set.add(node.val);
+
+    return dfs(node.left) || dfs(node.right);
+  }
+
+  return dfs(root);
 }

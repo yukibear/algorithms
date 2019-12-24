e57436d51e12cb71c00eec8766a2570db0f8a1d8
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-24 05:55:48 -0800

    #0002 init

---
+++ leetcode/solutions/0002.AddTwoNumbers/addTwoNumbers.ts
@@ -0,0 +1,9 @@
+import { SinglyLinkedListNode } from "../../../data_structures/SinglyLinkedListNode.ts";
+
+export default function addTwoNumbers(
+  l1: SinglyLinkedListNode<number> | null,
+  l2: SinglyLinkedListNode<number> | null
+): SinglyLinkedListNode<number> | null {
+
+  return null;
+}

f74363a3e3a99219bf0176f2f7f34ba2b817b002
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-23 19:21:14 -0800

    #0112 add BFS version

--- leetcode/solutions/0112.PathSum/hasPathSum_test.ts
+++ leetcode/solutions/0112.PathSum/hasPathSum_test.ts
@@ -1,16 +1,30 @@
 
 import { test } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import hasPathSum from "./hasPathSum.ts";
+import hasPathSum_DFS from "./hasPathSum_DFS.ts";
+import hasPathSum_BFS from "./hasPathSum_BFS.ts";
 import { createBinaryTreeNode } from "../../../data_structures/BinaryTreeNode.ts";
 
-test("0112. Path Sum", () => {
-  assertEquals(hasPathSum(
-    createBinaryTreeNode([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1]),
-    22
-  ), true);
-  assertEquals(hasPathSum(
-    createBinaryTreeNode([1]),
-    1
-  ), true);
-});
+const createTestFunction = (func: Function) => {
+  return () => {
+    assertEquals(func(
+      createBinaryTreeNode([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1]),
+      22
+    ), true);
+    assertEquals(func(
+      createBinaryTreeNode([1]),
+      1
+    ), true);
+    assertEquals(func(
+      createBinaryTreeNode([1, 2]),
+      1
+    ), false);
+    assertEquals(func(
+      createBinaryTreeNode([-2, null, -3]),
+      -5
+    ), true);
+  }
+};
+
+test("0112. Path Sum / DFS", createTestFunction(hasPathSum_DFS));
+test("0112. Path Sum / BFS", createTestFunction(hasPathSum_BFS));

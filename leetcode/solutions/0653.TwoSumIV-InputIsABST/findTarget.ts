aab34439749a301cdc9adce5e34f2c211b915d17
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-23 18:55:09 -0800

    #0653 add another solution to create sorted array and to use it

--- leetcode/solutions/0653.TwoSumIV-InputIsABST/findTarget.ts
+++ leetcode/solutions/0653.TwoSumIV-InputIsABST/findTarget.ts
@@ -1,6 +1,48 @@
 import { BinaryTreeNode } from "../../../data_structures/BinaryTreeNode.ts";
 
+// Inorder array of numbers solution
+// This solution makes the array sorted in ascending order first,
+// and then searchs a pair of numbers in it.
 export default function findTarget(
+  root: BinaryTreeNode<number> | null,
+  k: number
+): boolean {
+  const nums: number[] = [];
+  inorder(root, nums);
+
+  let i = 0;
+  let j = nums.length - 1;
+
+  while (i < j) {
+    const sum = nums[i] + nums[j];
+
+    if (sum === k) {
+      return true;
+    }
+
+    if (sum < k) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+
+  return false;
+}
+
+function inorder(node: BinaryTreeNode<number> | null, nums: number[]) {
+  if (!node) {
+    return;
+  }
+
+  inorder(node.left, nums);
+  nums.push(node.val);
+  inorder(node.right, nums);
+}
+
+// DFS solution where building a Set of TreeNode values
+// and checking if a difference(target - node.var) exists in the Set.
+export function findTarget1(
   root: BinaryTreeNode<number> | null,
   k: number
 ): boolean {
--- leetcode/solutions/0653.TwoSumIV-InputIsABST/findTarget_test.ts
+++ leetcode/solutions/0653.TwoSumIV-InputIsABST/findTarget_test.ts
@@ -1,15 +1,26 @@
 import { test } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import findTarget from "./findTarget.ts";
+import findTarget, { findTarget1 } from "./findTarget.ts";
 import { createBinaryTreeNode } from "../../../data_structures/BinaryTreeNode.ts";
 
-test("0653. Two Sum IV - Input is a BST", () => {
-  assertEquals(
-    findTarget(createBinaryTreeNode([5, 3, 6, 2, 4, null, 7]),
-    9
-  ), true);
-  assertEquals(
-    findTarget(createBinaryTreeNode([5, 3, 6, 2, 4, null, 7]),
-    28
-  ), false);
-});
+function createTest(func: Function) {
+  return () => {
+    assertEquals(
+      func(createBinaryTreeNode([5, 3, 6, 2, 4, null, 7]),
+      9
+    ), true);
+    assertEquals(
+      func(createBinaryTreeNode([5, 3, 6, 2, 4, null, 7]),
+      28
+    ), false);
+  }
+}
+
+test(
+  "0653. Two Sum IV - Input is a BST / DFS solution",
+  createTest(findTarget1)
+);
+test(
+  "0653. Two Sum IV - Input is a BST / Sorted array solution",
+  createTest(findTarget)
+);

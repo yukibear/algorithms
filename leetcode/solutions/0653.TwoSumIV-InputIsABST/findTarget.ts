a5ca13eb20021771a90fb5225dfb2a56a6a899c8
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-23 18:07:59 -0800

    #0653 init

---
+++ leetcode/solutions/0653.TwoSumIV-InputIsABST/findTarget.ts
@@ -0,0 +1,9 @@
+import { BinaryTreeNode } from "../../../data_structures/BinaryTreeNode.ts";
+
+export default function findTarget(
+  root: BinaryTreeNode<number> | null,
+  k: number
+): boolean {
+
+  return false;
+}

24ef6390f34e0ebc284152b96ba1e59e5aee6601
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-27 12:59:31 -0800

    #0012 solve

--- leetcode/solutions/0012.IntegerToRoman/intToRoman.ts
+++ leetcode/solutions/0012.IntegerToRoman/intToRoman.ts
@@ -1,4 +1,37 @@
-const hash = {
+export default function intToRoman(num: number): string {
+  if (num >= 5000) {
+    return "";
+  }
+
+  let str = "";
+
+  for (let digit of [1000, 100, 10, 1]) {
+    let n = Math.floor(num / digit);
+    num = num % digit;
+
+    if (n === 0) {
+      continue;
+    }
+    if (digit === 1000) {
+      str += INT_ROMAN_MAP[digit].repeat(n);
+      continue;
+    }
+
+    if (n === 9) {
+      str += INT_ROMAN_MAP[digit] + INT_ROMAN_MAP[digit * 10];
+    } else if (n >= 5) {
+      str +=  INT_ROMAN_MAP[digit * 5] + INT_ROMAN_MAP[digit].repeat(n - 5);
+    } else if (n === 4) {
+      str += INT_ROMAN_MAP[digit] + INT_ROMAN_MAP[digit * 5];
+    } else {
+      str += INT_ROMAN_MAP[digit].repeat(n);
+    }
+  }
+
+  return str;
+}
+
+const INT_ROMAN_MAP: Record<number, string> = {
   1: "I",
   5: "V",
   10: "X",
@@ -7,34 +40,3 @@ const hash = {
   500: "D",
   1000: "M"
 };
-
-  if (i === 9) {
-    str += hash[1] + hash[10];
-  } else if (i > =5) {
-    str +=  hash[5] + hash[1].repeat(i - 5);
-  } else {
-    str += hash[1].repeat(5 - i) + hash[5];
-  }
-
-/**
- * @param {number} num
- * @return {string}
- */
-export default function intToRoman(num: number): string {
-  let numArr: string[] = num.toString().split("");
-
-  let str = "";
-  for (let digit of [1000, 100, 10, 1]) {
-    let n = Math.floor(num / digit);
-
-    if (n === 9) {
-      str += hash[1] + hash[digit];
-    } else if (n >= 5) {
-      str +=  hash[5] + hash[1].repeat(n - 5);
-    } else if (n === 4) {
-      str += hash[1] + hash[5];
-    }
-  }
-
-  return str;
-}

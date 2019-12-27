017ce39a15b190f751183f715146f03eaed9010f
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-27 14:35:38 -0800

    #0200 solve

--- leetcode/solutions/0200.NumberOfIslands/numIslands.ts
+++ leetcode/solutions/0200.NumberOfIslands/numIslands.ts
@@ -1,10 +1,32 @@
-/**
- * @param {character[][]} grid
- * @return {number}
- */
-export default function numIslands(
-  grid: string[][]
-): number {
+export default function numIslands(grid: string[][]): number {
+  let islandCount = 0;
 
-  return 0;
+  function destroyIsland(i: number, j: number) {
+    if (!grid[i] || !grid[i][j] || grid[i][j] === "0") {
+      return;
+    }
+
+    grid[i][j] = "0";
+
+    destroyIsland(i + 1, j);
+    destroyIsland(i, j + 1);
+  }
+
+  function searchAndDestroyIsland(i: number, j: number) {
+    if (!grid[i] || !grid[i][j]) {
+      return;
+    }
+
+    if (grid[i][j] === "1") {
+      islandCount++;
+      destroyIsland(i, j);
+    }
+
+    searchAndDestroyIsland(i + 1, j);
+    searchAndDestroyIsland(i, j + 1);
+  }
+
+  searchAndDestroyIsland(0, 0);
+
+  return islandCount;
 }

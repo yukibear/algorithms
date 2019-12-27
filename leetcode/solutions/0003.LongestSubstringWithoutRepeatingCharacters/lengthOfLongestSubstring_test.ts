6ebc39907d581cfc69efa9d2a572640aa6403535
Yuki Sato <yuki.sato.dev@gmail.com>
2019-12-27 15:03:59 -0800

    #0003 refactor

--- leetcode/solutions/0003.LongestSubstringWithoutRepeatingCharacters/lengthOfLongestSubstring_test.ts
+++ leetcode/solutions/0003.LongestSubstringWithoutRepeatingCharacters/lengthOfLongestSubstring_test.ts
@@ -3,7 +3,7 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import lengthOfLongestSubstring from "./lengthOfLongestSubstring.ts";
 
 test("0003. Longest Substring Without Repeating Characters", () => {
-  // assertEquals(lengthOfLongestSubstring("abcabcbb"), 3);
+  assertEquals(lengthOfLongestSubstring("abcabcbb"), 3);
   assertEquals(lengthOfLongestSubstring("bbbbb"), 1);
   assertEquals(lengthOfLongestSubstring("pwwkew"), 3);
 });

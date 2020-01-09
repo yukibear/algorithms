0f722184b28e944f2ba49430ffefe5ce9bdf6806
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-08 20:13:05 -0800

    #0687 solve *but very slow. Refactoring is mandatory

--- leetcode/solutions/0687.LongestUnivaluePath/longestUnivaluePath.ts
+++ leetcode/solutions/0687.LongestUnivaluePath/longestUnivaluePath.ts
@@ -3,70 +3,59 @@ import { BinaryTreeNode } from "../../../data_structures/BinaryTreeNode.ts";
 export default function longestUnivaluePath(
   root: BinaryTreeNode<number> | null
 ): number {
-  dfs(root);
+  const numCountMap = new Map<number, number>([[0, 0]]);
 
-  return Math.max(...NUM_COUNT_MAP.values());
+  dfs(root, numCountMap);
+
+  return Math.max(...numCountMap.values());
 }
 
-function dfs(node: BinaryTreeNode<number> | null) {
+function dfs(
+  node: BinaryTreeNode<number> | null,
+  numCountMap: Map<number, number>
+): number {
   if (!node) {
     return 0;
   }
 
   const { left, right } = node;
-  let leftLen = 0;
-  let rightLen = 0;
+  let leftLen = dfs(left, numCountMap);
+  let rightLen = dfs(right, numCountMap);
+
+  if (left && right && left.val === right.val) {
+    if (left.val === node.val) {
+      leftLen++;
+      rightLen++;
+      numCountMap.set(
+        left.val,
+        Math.max(leftLen + rightLen, numCountMap.get(left.val) || 0)
+      );
+
+      return Math.max(leftLen, rightLen);
+    }
+
+    return 0;
+  }
 
   if (left && left.val === node.val) {
-    leftLen = dfs(left) + 1;
-  } else {
-    dfs(right);
+    leftLen++;
+    numCountMap.set(
+      left.val,
+      Math.max(leftLen, numCountMap.get(left.val) || 0)
+    );
+
+    return leftLen;
   }
 
   if (right && right.val === node.val) {
-    rightLen = dfs(right) + 1;
-  } else {
-    dfs(right);
+    rightLen++;
+    numCountMap.set(
+      right.val,
+      Math.max(rightLen, numCountMap.get(right.val) || 0)
+    );
+
+    return rightLen;
   }
 
-  if (left && right && left.val === right.val) {
-    const sum = leftLen + rightLen
-    NUM_COUNT_MAP.set(left.val, Math.max(sum, NUM_COUNT_MAP.get(left.val) || 0));
-  }
-
-  return Math.max(leftLen, rightLen);
+  return 0;
 }
-
-const NUM_COUNT_MAP = new Map<number, number>();
-
-
-
-
-
-
-// function countChildChainLength(node: BinaryTreeNode<number>): [number, number] {
-//   return [
-//     countChainLength(node.left, node.val),
-//     countChainLength(node.right, node.val)
-//   ];
-// }
-
-// function countChainLength(node: BinaryTreeNode<number> | null, parentVal: number) {
-//   if (!node) {
-//     return 0;
-//   }
-
-//   const [leftLen, rightLen] = countChildChainLength(node);
-//   const { val: currentVal } = node;
-
-//   if (currentVal !== parentVal) {
-//     NUM_COUNT_MAP.set(
-//       currentVal,
-//       Math.max(maxChainLength, NUM_COUNT_MAP.get(currentVal) || 0)
-//     );
-
-//     return 0;
-//   }
-
-//   return maxChainLength + 1;
-// }

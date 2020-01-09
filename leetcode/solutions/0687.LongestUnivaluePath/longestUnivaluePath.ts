5030eb1fa73b69d7556d9a5158bb815c93a3bb5a
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-09 01:33:34 -0800

    #0687 refactor

--- leetcode/solutions/0687.LongestUnivaluePath/longestUnivaluePath.ts
+++ leetcode/solutions/0687.LongestUnivaluePath/longestUnivaluePath.ts
@@ -3,59 +3,29 @@ import { BinaryTreeNode } from "../../../data_structures/BinaryTreeNode.ts";
 export default function longestUnivaluePath(
   root: BinaryTreeNode<number> | null
 ): number {
-  const numCountMap = new Map<number, number>([[0, 0]]);
+  // Using an array as just a refference of one maximum result to make code simpl.
+  // Thus its length is always 1.
+  const max: [number] = [0];
 
-  dfs(root, numCountMap);
+  dfs(root, max);
 
-  return Math.max(...numCountMap.values());
+  return max[0];
 }
 
-function dfs(
-  node: BinaryTreeNode<number> | null,
-  numCountMap: Map<number, number>
-): number {
+function dfs(node: BinaryTreeNode<number> | null, max: [number]): number {
   if (!node) {
     return 0;
   }
 
   const { left, right } = node;
-  let leftLen = dfs(left, numCountMap);
-  let rightLen = dfs(right, numCountMap);
 
-  if (left && right && left.val === right.val) {
-    if (left.val === node.val) {
-      leftLen++;
-      rightLen++;
-      numCountMap.set(
-        left.val,
-        Math.max(leftLen + rightLen, numCountMap.get(left.val) || 0)
-      );
+  let leftLen = dfs(left, max);
+  let rightLen = dfs(right, max);
 
-      return Math.max(leftLen, rightLen);
-    }
+  leftLen = (left && left.val === node.val) ? leftLen + 1 : 0;
+  rightLen = (right && right.val === node.val) ? rightLen + 1 : 0;
 
-    return 0;
-  }
+  max[0] = Math.max(max[0], leftLen + rightLen);
 
-  if (left && left.val === node.val) {
-    leftLen++;
-    numCountMap.set(
-      left.val,
-      Math.max(leftLen, numCountMap.get(left.val) || 0)
-    );
-
-    return leftLen;
-  }
-
-  if (right && right.val === node.val) {
-    rightLen++;
-    numCountMap.set(
-      right.val,
-      Math.max(rightLen, numCountMap.get(right.val) || 0)
-    );
-
-    return rightLen;
-  }
-
-  return 0;
+  return Math.max(leftLen, rightLen);
 }

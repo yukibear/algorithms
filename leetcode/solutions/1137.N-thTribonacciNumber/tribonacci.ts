80e2dd0464495435ab1a590f635c322b441e7c0b
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-10 15:13:26 -0800

    #1137 refactor

--- leetcode/solutions/1137.N-thTribonacciNumber/tribonacci.ts
+++ leetcode/solutions/1137.N-thTribonacciNumber/tribonacci.ts
@@ -1,15 +1,12 @@
 export default function tribonacci(n: number): number {
-  const dp: [number, number, number] = [0, 1, 1];
+  let dp: [number, number, number] = [0, 1, 1];
 
   if (n < 3) {
     return dp[n];
   }
 
   for (let i = 3; i <= n; i++) {
-    const num = dp[0] + dp[1] + dp[2];
-    dp[0] = dp[1];
-    dp[1] = dp[2];
-    dp[2] = num;
+    dp = [dp[1], dp[2], dp[0] + dp[1] + dp[2]];
   }
 
   return dp[2];

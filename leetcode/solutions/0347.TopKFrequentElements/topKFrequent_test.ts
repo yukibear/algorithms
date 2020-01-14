986f5aedc27e546cffa4c1a41eeac41611175002
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-14 00:41:27 -0800

    #0347 rename files

--- leetcode/solutions/0347.TopKFrequentElements/topKFrequent_test.ts
+++ leetcode/solutions/0347.TopKFrequentElements/topKFrequent_test.ts
@@ -1,6 +1,6 @@
 import { test } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import topKFrequent from "./topKFrequentElements.ts";
+import topKFrequent from "./topKFrequent.ts";
 
 test("0347. Top K Frequent Elements", () => {
   assertEquals(topKFrequent([1, 1, 1, 2, 2, 3], 2), [1, 2]);

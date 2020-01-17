e007aa94c8971338a0bb1581bad5a3a0aa2c6614
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-16 22:56:22 -0800

    #0009 add string conversion ver

--- leetcode/solutions/0009.PalindromeNumber/isPalindrome.ts
+++ leetcode/solutions/0009.PalindromeNumber/isPalindrome.ts
@@ -18,3 +18,20 @@ export default function isPalindrome(x: number): boolean {
 
   return true;
 }
+
+export function isPalindrome_Str(x: number): boolean {
+  if (x < 0) {
+    return false;
+  }
+
+  const str = x.toString();
+  const mid = Math.floor(str.length / 2);
+
+  for (let i = 0; i <= mid; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}

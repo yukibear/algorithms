8ba8734cbd6bba3047ad6b3f77eea93b3cf0cbf1
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-17 17:45:43 -0800

    #0036 refactor

--- leetcode/solutions/0036.ValidSudoku/isValidSudoku.ts
+++ leetcode/solutions/0036.ValidSudoku/isValidSudoku.ts
@@ -1,73 +1,30 @@
 export default function isValidSudoku(board: string[][]): boolean {
+  const seen = new Set<string>();
 
   for (let i = 0; i < 9; i++) {
-    if (!rowIsValid(board[i])
-      || !colIsValid(board, i)
-      || !subBoardIsValid(board, i)
-    ) {
-      return false;
-    }
-  }
+    for (let j = 0; j < 9; j++) {
+      const char = board[i][j];
 
-  return true;
-}
-
-function rowIsValid(row: string[]): boolean {
-  const set = new Set<string>();
-
-  for (const char of row) {
-    if (char === DOT) {
-      continue;
-    }
-    if (set.has(char)) {
-      return false;
-    }
-    set.add(char);
-  }
-
-  return true;
-}
-
-function colIsValid(board: string[][], col: number): boolean {
-  const set = new Set<string>();
-
-  for (let i = 0; i < 9; i++) {
-    const char = board[i][col];
-
-    if (char === DOT) {
-      continue;
-    }
-    if (set.has(char)) {
-      return false;
-    }
-
-    set.add(char);
-  }
-
-  return true;
-}
-
-function subBoardIsValid(board: string[][], subBoardIndex: number): boolean {
-  const set = new Set<string>();
-  let rowStart = Math.floor(subBoardIndex / 3) * 3;
-  let colStart = subBoardIndex % 3 * 3;
-
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      const char = board[rowStart + i][colStart + j];
-
-      if (char === DOT) {
+      if (char === EMPTY) {
         continue;
       }
-      if (set.has(char)) {
+
+      const rowKey = `${char} in row ${i.toString()}`;
+      const colKey = `${char} in column ${j.toString()}`;
+      const blockKey = `${char} in block `
+        + Math.floor(i / 3) + "-" + Math.floor(j / 3);
+
+      if (seen.has(rowKey) || seen.has(colKey) || seen.has(blockKey)) {
         return false;
       }
 
-      set.add(char);
+      seen.add(rowKey);
+      seen.add(colKey);
+      seen.add(blockKey);
     }
   }
 
   return true;
 }
 
-const DOT = ".";
+const EMPTY = ".";

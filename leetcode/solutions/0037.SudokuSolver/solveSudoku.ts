09d17aeb8caacedee496569ee165d768eab80692
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-18 15:02:23 -0800

    #0037 WIP

--- leetcode/solutions/0037.SudokuSolver/solveSudoku.ts
+++ leetcode/solutions/0037.SudokuSolver/solveSudoku.ts
@@ -1,3 +1,27 @@
 export default function solveSudoku(board: string[][]): void {
+  const rowCandidates = Array.from({ length: 9 }, _ => new Set(ZERO_TO_NINE));
+  const colCandidates = Array.from({ length: 9 }, _ => new Set(ZERO_TO_NINE));
+  const blockCandidates = Array.from({ length: 9 }, _ => new Set(ZERO_TO_NINE));
 
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const char = board[i][j];
+
+      if (char === EMPTY) {
+        continue;
+      }
+
+      rowCandidates[i].delete(char);
+      colCandidates[j].delete(char);
+
+      const blockIndex = Math.floor(i / 3) * Math.floor(j / 3);
+      blockCandidates[blockIndex].delete(char);
+    }
+  }
+
+  for (const blockCandidate of blockCandidates) {
+  }
 }
+
+const ZERO_TO_NINE = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const EMPTY = ".";

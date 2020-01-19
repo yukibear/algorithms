cbcf0b1b3540fd317a9c5a0e096529f2543c3e93
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-19 13:13:03 -0800

    #5315 refactor not to use string conversion & spli and use number of digits instead

--- leetcode/solutions/5315.Maximum69Number/maximum69Number.ts
+++ leetcode/solutions/5315.Maximum69Number/maximum69Number.ts
@@ -1,12 +1,17 @@
 export default function maximum69Number(num: number): number {
-  const n = num.toString().split("");
+  const numOfDigits = Math.floor(Math.log10(num)) + 1;
+  let restNum = num;
 
-  for (let i = 0; i < n.length; i++) {
-    if (n[i] === "6") {
-      n[i] = "9";
+  for (let i = numOfDigits - 1; i >= 0; i--) {
+    let base = 10 ** i;
+
+    if (Math.floor(restNum / base)  === 6) {
+      num += 3 * base;
       break;
     }
+
+    restNum = restNum % base;
   }
 
-  return parseInt(n.join(""));
+  return num;
 }
--- leetcode/solutions/5315.Maximum69Number/maximum69Number_test.ts
+++ leetcode/solutions/5315.Maximum69Number/maximum69Number_test.ts
@@ -4,4 +4,6 @@ import maximum69Number from "./maximum69Number.ts";
 
 test("5315. Maximum 69 Number", () => {
   assertEquals(maximum69Number(9669), 9969);
+  assertEquals(maximum69Number(9996), 9999);
+  assertEquals(maximum69Number(9999), 9999);
 });

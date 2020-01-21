0c6cddd1943006a629ee6da9d097247f36013386
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-20 19:04:39 -0800

    #0153 solve

---
+++ leetcode/solutions/0153.FindMinimumInRotatedSortedArray/findMin_test.ts
@@ -0,0 +1,8 @@
+import { test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import findMin from "./findMin.ts";
+
+test("0153. Find Minimum in Rotated Sorted Array", () => {
+  assertEquals(findMin([3, 4, 5, 1, 2]), 1);
+  assertEquals(findMin([4, 5, 6, 7, 0, 1, 2]), 0);
+});

0ef5215c87be8d769123a65bb619e153a501f401
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-26 00:27:13 -0800

    #1326 solve

--- leetcode/solutions/1326.MinimumNumberOfTapsToOpenToWaterAGarden/minTaps.ts
+++ leetcode/solutions/1326.MinimumNumberOfTapsToOpenToWaterAGarden/minTaps.ts
@@ -1,70 +1,37 @@
 export default function minTaps(n: number, ranges: number[]): number {
-  const dp: number[] = [];
-  let toCover: number | null = null;
-
-  for (const i of ranges.keys()) {
-    const range = ranges[i];
-    console.log(dp);
-
-    if (range === 0) {
-      if (dp[i] == null && toCover == null) {
-        toCover = i;
-      }
-
+  for (const [i, range] of ranges.entries()) {
+    if (i === 0) {
       continue;
     }
 
-    // if a range covers all previous area
-    if (range >= i) {
-      dp[i] = 1;
-      toCover = null;
-      fillFarther(dp, i, range, dp[i]);
-
-      continue;
-    }
-
-    if (dp[i] == null) {
-      if (toCover != null && toCover >= i - range) {
-        toCover = null;
-      }
-
-      if (dp[i - range - 1] != null) {
-        dp[i] = dp[i - range - 1] + 1;
-        fillFarther(dp, i, range, dp[i]);
-      }
-
-      continue;
-    }
-    if (i === 6) {
-      console.log(i, range, dp[i], dp[i - range - 1]);
-    }
-    if (dp[i - range - 1] != null) {
-      if (dp[i] >= dp[i - range - 1] + 1) {
-        dp[i] = dp[i - range - 1] + 1;
-        fillFarther(dp, i, range, dp[i]);
-      } else {
-        console.log(i, dp[i] + 1, range);
-
-        fillFarther(dp, i, range, dp[i] + 1);
-      }
-    }
-  }
-  console.log(toCover, dp);
-
-  return toCover === null ? dp[n] : -1;
-}
-
-function fillFarther(
-  dp: number[],
-  fromIndex: number,
-  range: number,
-  count: number
-) {
-  for (let i = 1; i <= range; i++) {
-    if (dp[fromIndex + i] == null) {
-      dp[fromIndex + i] = count;
+    if (ranges[i] > i) {
+      ranges[0] = Math.max(ranges[0], range + i);
     } else {
-      dp[fromIndex + i] = Math.min(dp[fromIndex + i], count);
+      const from = i - ranges[i];
+      ranges[from] = Math.max(ranges[from], range * 2);
+    }
+
+    ranges[i] = 0;
+  }
+
+  let count = 0;
+  let farthest = 0;
+  let lastFarthest = 0;
+
+  for (const [i, range] of ranges.entries()) {
+    if (range !== 0) {
+      farthest = Math.max(farthest, i + range);
+    }
+
+    if (i === lastFarthest && i !== ranges.length - 1) {
+      if (farthest === i) {
+        return -1;
+      }
+
+      count++;
+      lastFarthest = farthest;
     }
   }
+
+  return count;
 }
--- leetcode/solutions/1326.MinimumNumberOfTapsToOpenToWaterAGarden/minTaps_test.ts
+++ leetcode/solutions/1326.MinimumNumberOfTapsToOpenToWaterAGarden/minTaps_test.ts
@@ -3,13 +3,13 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import minTaps from "./minTaps.ts";
 
 test("1326. Minimum Number of Taps to Open to Water a Garden", () => {
-  // assertEquals(minTaps(5, [3, 4, 1, 1, 0, 0]), 1);
-  // assertEquals(minTaps(3, [0, 0, 0, 0]), -1);
-  // assertEquals(minTaps(7, [1, 2, 1, 0, 2, 1, 0, 1]), 3);
-  // assertEquals(minTaps(8, [4, 0, 0, 0, 0, 0, 0, 0, 4]), 2);
-  // assertEquals(minTaps(8, [4, 0, 0, 0, 4, 0, 0, 0, 4]), 1);
-  // assertEquals(minTaps(9, [0, 5, 0, 3, 3, 3, 1, 4, 0, 4]), 2);
-  // assertEquals(minTaps(17, [0,3,3,2,2,4,2,1,5,1,0,1,2,3,0,3,1,1]), 3);
+  assertEquals(minTaps(5, [3, 4, 1, 1, 0, 0]), 1);
+  assertEquals(minTaps(3, [0, 0, 0, 0]), -1);
+  assertEquals(minTaps(7, [1, 2, 1, 0, 2, 1, 0, 1]), 3);
+  assertEquals(minTaps(8, [4, 0, 0, 0, 0, 0, 0, 0, 4]), 2);
+  assertEquals(minTaps(8, [4, 0, 0, 0, 4, 0, 0, 0, 4]), 1);
+  assertEquals(minTaps(9, [0, 5, 0, 3, 3, 3, 1, 4, 0, 4]), 2);
+  assertEquals(minTaps(17, [0,3,3,2,2,4,2,1,5,1,0,1,2,3,0,3,1,1]), 3);
   assertEquals(
     minTaps(35, [
       1,

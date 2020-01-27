54ab9f4224a1408e85fea43ae32e934227637e2a
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-27 00:53:44 -0800

    #1332 solve

--- leetcode/solutions/1332.RemovePalindromicSubsequences/removePalindromeSub.ts
+++ leetcode/solutions/1332.RemovePalindromicSubsequences/removePalindromeSub.ts
@@ -1,29 +1,9 @@
 export default function removePalindromeSub(s: string): number {
-  let count = 0;
-
-  while (s.length > 0) {
-    let half = Math.floor(s.length / 2);
-
-    for (let end = half; end >= 0; end--) {
-      const str1 = s.slice(0, end);
-      if (str1 === "") {
-        count++;
-        return count;
-      }
-      const str2 = str1
-        .split("")
-        .reverse()
-        .join("");
-
-      const lastIndex = s.lastIndexOf(str2, str1.length);
-      if (lastIndex === -1) {
-        continue;
-      }
-      s = s.substring(0, lastIndex) + s.substring(lastIndex + str2.length);
-      s = s.substring(str1.length);
-      count++;
-    }
+  if (s.length === 0) {
+    return 0;
   }
 
-  return count;
+  const seek = s.substr(Math.ceil(s.length / 2)).split("").reverse().join("");
+
+  return s.indexOf(seek) === 0 ? 1 : 2;
 }

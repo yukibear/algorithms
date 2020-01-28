fe1bf980422c7a9c90a6e260e8932b90399dadd5
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-28 10:03:03 -0800

    Delete unused files

--- 0643.MaximumAverageSubarrayI/findMaxAverage_test.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "https://deno.land/std/testing/mod.ts";
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import findMaxAverage from "./findMaxAverage.ts";
-
-test("0643. Maximum Average Subarray I", () => {
-  assertEquals(findMaxAverage([1, 12, -5, -6, 50, 3], 4), 12.75);
-});

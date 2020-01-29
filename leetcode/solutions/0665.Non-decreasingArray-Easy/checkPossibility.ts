0c614c507578baebc5189db2799f92ae4c10f690
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-29 00:52:58 -0800

    #0665 solve

---
+++ leetcode/solutions/0665.Non-decreasingArray-Easy/checkPossibility.ts
@@ -0,0 +1,28 @@
+export default function checkPossibility(nums: number[]): boolean {
+  if (nums.length <= 2) {
+    return true;
+  }
+
+  let hasDecreced = false;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] <= nums[i]) {
+      continue;
+    }
+
+    if (hasDecreced) {
+      return false;
+    }
+
+    if (
+      nums[i - 1] > nums[i + 1]
+      && (nums[i - 2] != null && nums[i - 2] > nums[i])
+    ) {
+      return false;
+    }
+
+    hasDecreced = true;
+  }
+
+  return true;
+}
---
+++ leetcode/solutions/0665.Non-decreasingArray-Easy/checkPossibility_test.ts
@@ -0,0 +1,9 @@
+import { test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import checkPossibility from "./checkPossibility.ts";
+
+test("0665. Non-decreasing Array - Easy", () => {
+  assertEquals(checkPossibility([4, 2, 3]), true);
+  assertEquals(checkPossibility([4, 2, 1]), false);
+  assertEquals(checkPossibility([3,4,2,3]), false);
+});

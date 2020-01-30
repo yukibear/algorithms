019be81532de2329244ea11529b00877f4b935db
Yuki Sato <yuki.sato.dev@gmail.com>
2020-01-30 00:05:15 -0800

    #0997 refactor

--- leetcode/solutions/0997.FindTheTownJudge/findJudge.ts
+++ leetcode/solutions/0997.FindTheTownJudge/findJudge.ts
@@ -1,33 +1,16 @@
 export default function findJudge(N: number, trust: number[][]): number {
-  if (trust.length === 0) {
-    return 1
-  }
-
-  const trusted = new Map();
-  const trusts = new Map();
+  const count = Array.from({ length: N + 1 }, () => 0);
 
   for (const [from, to] of trust) {
-    trusted.set(to, (trusted.get(to) || new Set<number>()).add(from));
-    trusts.set(from, (trusts.get(from) || new Set<number>()).add(to));
+    count[from]--;
+    count[to]++;
   }
 
-  if (trusts.size === N) {
-    return -1;
-  }
-
-  let townJudge = 0;
-
-  for (let i = 1; i <= N; i++) {
-    if (trusts.has(i) || !trusted.has(i) || trusted.get(i).size < N - 1) {
-      continue;
+  for (let i = 1; i < count.length; i++) {
+    if (count[i] === N - 1) {
+      return i;
     }
-
-    if (townJudge) {
-      return -1;
-    }
-
-    townJudge = i;
   }
 
-  return townJudge === 0 ? -1 : townJudge;
+  return -1;
 }

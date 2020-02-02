977793223b83c6f7e2b704f6e189a57ec5bb765a
Yuki Sato <yuki.sato.dev@gmail.com>
2020-02-01 23:43:35 -0800

    #1341 refactor

--- leetcode/solutions/1341.TheKWeakestRowsInAMatrix/kWeakestRows.ts
+++ leetcode/solutions/1341.TheKWeakestRowsInAMatrix/kWeakestRows.ts
@@ -1,19 +1,22 @@
 export default function kWeakestRows(mat: number[][], k: number): number[] {
-  const map = new Map<number, number[]>();
-  mat.map((arr, i) => {
-    const count = arr.filter(n => n === 1).length;
-    const a = map.get(count) || [];
-    a.push(i);
-    map.set(count, a);
-  });
+  const soldirCountMap = new Map<number, number[]>();
 
-  const mapKeys = [...map.keys()].sort((a, b) => a - b);
+  for (const [i, row] of mat.entries()) {
+    const count = row.reduce((p, n) => p + n);
+    const counts = soldirCountMap.get(count) || [];
+    counts.push(i);
+    soldirCountMap.set(count, counts);
+  }
+
+  const sortedSoldirCuonts = [...soldirCountMap.entries()].sort(([a,], [b,]) => a - b);
   const res: number[] = [];
 
-  for (const key of mapKeys) {
-    const ke = map.get(key)!.sort((a, b) => a - b);
-    for (const i of ke) {
+  for (const [, indices] of sortedSoldirCuonts) {
+    indices.sort((a, b) => a - b);
+
+    for (const i of indices) {
       res.push(i);
+
       if (res.length === k) {
         return res;
       }

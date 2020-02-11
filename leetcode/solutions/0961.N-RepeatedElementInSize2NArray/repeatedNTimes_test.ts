ccd52f784b8eaddf52ac7e9c83b9a4ba6ed451a4
Yuki Sato <yuki.sato.dev@gmail.com>
2020-02-11 15:56:00 -0800

    #0961 solve

---
+++ leetcode/solutions/0961.N-RepeatedElementInSize2NArray/repeatedNTimes_test.ts
@@ -0,0 +1,9 @@
+import { test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import repeatedNTimes from "./repeatedNTimes.ts";
+
+test("0961. N-Repeated Element in Size 2N Array", () => {
+  assertEquals(repeatedNTimes([1, 2, 3, 3]), 3);
+  assertEquals(repeatedNTimes([2, 1, 2, 5, 3, 2]), 2);
+  assertEquals(repeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4]), 5);
+});

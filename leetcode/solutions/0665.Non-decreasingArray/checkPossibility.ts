84f1b0d71dd5b0cec8a295de79cc2f5d40c5be95
Steve <steve@ecpmail.com>
2020-02-12 17:54:27 -0800

    #0665 refactor

--- leetcode/solutions/0665.Non-decreasingArray/checkPossibility.ts
+++ leetcode/solutions/0665.Non-decreasingArray/checkPossibility.ts
@@ -1,8 +1,4 @@
 export default function checkPossibility(nums: number[]): boolean {
-  if (nums.length <= 2) {
-    return true;
-  }
-
   let hasDecreced = false;
 
   for (let i = 1; i < nums.length; i++) {
@@ -14,12 +10,10 @@ export default function checkPossibility(nums: number[]): boolean {
       return false;
     }
 
-    if (
-      nums[i - 1] > nums[i + 1] &&
-      nums[i - 2] != null &&
-      nums[i - 2] > nums[i]
-    ) {
-      return false;
+    if (i >= 2 && nums[i - 2] > nums[i]) {
+      nums[i] = nums[i - 1];
+    } else {
+      nums[i - 1] = nums[i];
     }
 
     hasDecreced = true;

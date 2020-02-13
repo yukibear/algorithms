9420b62fb9012ab31fde9d073a83b543da02e966
Steve <steve@ecpmail.com>
2020-02-12 17:55:28 -0800

    #0665 solve with Go

---
+++ leetcode/solutions/0665.Non-decreasingArray/checkPossibility_test.go
@@ -0,0 +1,33 @@
+package solution
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func Test_0665_NonDecreasingArray(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		in  []int
+		out bool
+	}{
+		{[]int{4, 2, 3}, true},
+		{[]int{4, 2, 1}, false},
+		{[]int{3, 4, 2, 3}, false},
+	}
+
+	for i, tt := range tests {
+		_, tt := i, tt
+
+		t.Run("Test", func(t *testing.T) {
+			got := checkPossibility(tt.in)
+
+			if !cmp.Equal(got, tt.out) {
+				t.Errorf("TEST FAILED! got: %#v, want: %#v.", got, tt.out)
+			}
+		})
+	}
+
+}

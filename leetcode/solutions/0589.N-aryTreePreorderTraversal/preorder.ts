58e1f91631594093e145123cd3ccd1d2a95c776d
Yuki Sato <yuki.sato.dev@gmail.com>
2020-02-15 16:52:25 -0800

    #0589 solve

---
+++ leetcode/solutions/0589.N-aryTreePreorderTraversal/preorder.ts
@@ -0,0 +1,26 @@
+export interface Node {
+  val: number;
+  children: Node[]
+}
+
+export default function preorder(
+  root: Node
+): number[] {
+  const reuslt: number[] = [];
+
+  function dfs(node: Node | null): void {
+    if (!node) {
+      return;
+    }
+
+    reuslt.push(node.val);
+
+    for (const child of node.children) {
+      dfs(child);
+    }
+  }
+
+  dfs(root);
+
+  return reuslt;
+}
---
+++ leetcode/solutions/0589.N-aryTreePreorderTraversal/preorder_test.ts
@@ -0,0 +1,21 @@
+import { test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import preorder, { Node } from "./preorder.ts";
+
+test("0589. N-ary Tree Preorder Traversal", () => {
+  const node: Node = {
+    val: 1,
+    children: [
+      {
+        val: 3,
+        children: [
+          { val: 5, children: [] },
+          { val: 6, children: [] }
+        ]
+      },
+      { val: 2, children: [] },
+      { val: 4, children: [] }
+    ]
+  };
+  assertEquals(preorder(node), [1, 3, 5, 6, 2, 4]);
+});

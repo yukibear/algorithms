ea2704d5c97f6792114f60880ce47442f6b82314
Yuki Sato <yuki.sato.dev@gmail.com>
2020-03-03 01:48:18 -0800

    #1289 refactor

--- leetcode/solutions/1289.MinimumFallingPathSumII/minFallingPathSum.ts
+++ leetcode/solutions/1289.MinimumFallingPathSumII/minFallingPathSum.ts
@@ -6,30 +6,33 @@ export default function minFallingPathSum(
   );
 
   for (let i = 1; i < arr.length; i++) {
-    let tmp: number[][] = [[0, Infinity], [0, Infinity]];
+    let minSums: number[][] = [[0, Infinity], [0, Infinity]];
 
     for (let j = 0; j < arr.length; j++) {
-      const num = arr[i][j];
-      let sum = num;
-
       if (dp[0][0] !== arr[i - 1][j]) {
-        sum += dp[0][1];
+        updateMinValue(minSums, arr[i][j], dp[0][1]);
       } else {
-        sum += dp[1][1];
-      }
-
-      if (sum < tmp[1][1]) {
-        if (sum < tmp[0][1]) {
-          tmp[1] = tmp[0];
-          tmp[0] = [num, sum];
-        } else {
-          tmp[1] = [num, sum];
-        }
+        updateMinValue(minSums, arr[i][j], dp[1][1]);
       }
     }
 
-    dp = tmp;
+    dp = minSums;
   }
 
   return dp[0][1];
+}
+
+function updateMinValue(minSums: number[][], num: number, prevSum: number) {
+  let sum = num + prevSum;
+
+  if (sum < minSums[1][1]) {
+    if (sum < minSums[0][1]) {
+      minSums[1] = minSums[0];
+      minSums[0] = [num, sum];
+    } else {
+      minSums[1] = [num, sum];
+    }
+  }
+
+  return minSums;
 }

a3dea636824fd7d1f8546511664bea907624746a
Yuki Sato <yuki.sato.dev@gmail.com>
2020-03-17 12:36:12 -0700

    #5357 solve

---
+++ leetcode/solutions/5357.DesignAStackWithIncrementOperation/CustomStack_test.ts
@@ -0,0 +1,21 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import CustomStack from "./CustomStack.ts";
+
+Deno.test("5357. Design a Stack With Increment Operation", () => {
+  const customStack = new CustomStack(3);
+  customStack.push(1);
+  customStack.push(2);
+
+  assertEquals(customStack.pop(), 2);
+
+  customStack.push(2);
+  customStack.push(3);
+  customStack.push(4);
+  customStack.increment(5, 100);
+  customStack.increment(2, 100);
+
+  assertEquals(customStack.pop(), 103);
+  assertEquals(customStack.pop(), 202);
+  assertEquals(customStack.pop(), 201);
+  assertEquals(customStack.pop(), -1);
+});

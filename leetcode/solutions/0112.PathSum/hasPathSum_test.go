72ef2b79659ac837c23d73f2de80712a544eb5cf
Yuki Sato <yuki.sato.dev@gmail.com>
2020-03-31 00:12:59 -0700

    #0112 create tests. Also refactor a logic of the solution

---
+++ leetcode/solutions/0112.PathSum/hasPathSum_test.go
@@ -0,0 +1,62 @@
+package solution
+
+import (
+	"testing"
+
+	ds "../../../datastructure"
+	"github.com/google/go-cmp/cmp"
+)
+
+func Test_hoge(t *testing.T) {
+	type in = struct {
+		arg1 *TreeNode
+		arg2 int
+	}
+	tests := []struct {
+		in  in
+		out bool
+	}{
+		{
+			in{
+				nil,
+				0,
+			},
+			false,
+		},
+		{
+			in{
+				ds.CreateBinaryTreeNodeFromString([]string{"1"}),
+				1,
+			},
+			true,
+		},
+		{
+			in{
+				ds.CreateBinaryTreeNodeFromString([]string{"1", "2"}),
+				1,
+			},
+			false,
+		},
+		{
+			in{
+				ds.CreateBinaryTreeNodeFromString([]string{"5", "4", "8", "11", "null", "13", "4", "7", "2", "null", "null", "null", "1"}),
+				22,
+			},
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run("Test", func(t *testing.T) {
+			t.Parallel()
+
+			got := hasPathSum(tt.in.arg1, tt.in.arg2)
+
+			if !cmp.Equal(got, tt.out) {
+				t.Errorf("got %#v want %#v", got, tt.out)
+			}
+		})
+	}
+}

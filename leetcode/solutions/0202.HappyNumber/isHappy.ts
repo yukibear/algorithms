e7a2911be482a50408e255dbb2cd71d9229e1ce2
Yuki Sato <yuki.sato.dev@gmail.com>
2020-04-02 12:42:44 -0700

    #0202 refactor

--- leetcode/solutions/0202.HappyNumber/isHappy.ts
+++ leetcode/solutions/0202.HappyNumber/isHappy.ts
@@ -1,24 +1,19 @@
 export default function isHappy(n: number): boolean {
   const set = new Set<number>();
 
-  while (true) {
+  while (!set.has(n)) {
+    // To break inifinite loop.
+    set.add(n);
+
     let sum = 0;
 
     while (n > 0) {
-      const num = n % 10;
-      sum += num ** 2;
+      sum += (n % 10) ** 2;
       n = Math.floor(n / 10);
     }
 
-    if (sum === 1) {
-      return true;
-    }
-
-    if (set.has(sum)) {
-      return false;
-    }
-
-    set.add(sum);
     n = sum;
   }
+
+  return n === 1;
 }

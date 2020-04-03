26fb078df6b10664114ce7c496cf906f89a31ecb
Yuki Sato <yuki.sato.dev@gmail.com>
2020-04-03 02:48:13 -0700

    #0235 solve by both TS and Go

---
+++ leetcode/solutions/0235.LowestCommonAncestorOfABinarySearchTree/lowestCommonAncestor.ts
@@ -0,0 +1,23 @@
+import { BinaryTreeNode } from "../../../datastructure/BinaryTreeNode.ts";
+
+export default function lowestCommonAncestor(
+  root: BinaryTreeNode<number> | null,
+  p: BinaryTreeNode<number> | null,
+  q: BinaryTreeNode<number> | null
+): BinaryTreeNode<number> | null {
+  if (root === null || p === null || q === null) {
+    return null;
+  }
+
+  if (root === p || root === q) {
+    return root;
+  }
+
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
+
+  return root;
+}

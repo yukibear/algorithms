b4b60f3a29e51dd1d29321c5f366e14d3d1ab244
Yuki Sato <yuki.sato.dev@gmail.com>
2020-04-03 02:01:01 -0700

    #0053 improve test case and code written in TS

--- leetcode/solutions/0053.MaximumSubarray/maxSubArray.ts
+++ leetcode/solutions/0053.MaximumSubarray/maxSubArray.ts
@@ -4,10 +4,7 @@ export default function maxSubArray(nums: number[]): number {
 
   for (let i = 1; i < nums.length; i++) {
     prevMax = Math.max(prevMax + nums[i], nums[i]);
-
-    if (prevMax > max) {
-      max = prevMax;
-    }
+    max = Math.max(max, prevMax);
   }
 
   return max;
--- leetcode/solutions/0053.MaximumSubarray/maxSubArray_test.ts
+++ leetcode/solutions/0053.MaximumSubarray/maxSubArray_test.ts
@@ -2,5 +2,18 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import maxSubArray from "./maxSubArray.ts";
 
 Deno.test("0053. Maximum Subarray", () => {
-  assertEquals(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
+  type test = [number[], number];
+  const tests: test[] = [
+    [[-1], -1],
+    [[-3, -2, -3, -4], -2],
+    [[0, 0, 0, 0, 0], 0],
+    [[1, 1, 1, 1, 1], 5],
+    [[1, -1, 1, -1, 1], 1],
+    [[1, -1, 2, -1, 2], 3],
+    [[-2, 1, -3, 4, -1, 2, 1, -5, 4], 6],
+  ];
+
+  for (const [arg1, want] of tests) {
+    assertEquals(maxSubArray(arg1), want);
+  }
 });

787bc806ba0a473f7fcb74a7508b7f5dc5feeae0
Yuki Sato <yuki.sato.dev@gmail.com>
2020-04-04 00:55:51 -0700

    #0283 improve testing style

--- leetcode/solutions/0283.MoveZeroes/moveZeroes_test.ts
+++ leetcode/solutions/0283.MoveZeroes/moveZeroes_test.ts
@@ -1,13 +1,22 @@
-import { test } from "https://deno.land/std/testing/mod.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import moveZeroes from "./moveZeroes.ts";
 
-test("0283. Move Zeroes", () => {
-  const arr1 = [0, 1, 0, 3, 12];
-  moveZeroes(arr1)
-  assertEquals(arr1, [1, 3, 12, 0, 0]);
+Deno.test("0283. Move Zeroes", () => {
+  type test = [
+    number[],
+    number[],
+  ];
+  const tests: test[] = [
+		[[0], [0]],
+		[[1], [1]],
+		[[0, 0, 0], [0, 0, 0]],
+		[[0, 1, 0], [1, 0, 0]],
+    [[3, 2, 1], [3, 2, 1]],
+    [[0, 1, 0, 3, 12], [1, 3, 12, 0, 0]],
+  ]
 
-  const arr2 = [1];
-  moveZeroes(arr2)
-  assertEquals(arr2, [1]);
+  for (const [arg1, want] of tests) {
+    moveZeroes(arg1);
+    assertEquals(arg1, want);
+  }
 });

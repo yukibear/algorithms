08f7d54013397167fbfbd3d1e16061462916b03f
Yuki Sato <yuki.sato.dev@gmail.com>
2020-04-08 00:11:37 -0700

    #0049 refactor code to use [26]int as key instead of using char-sorted strings in order to improve performance

--- leetcode/solutions/0049.GroupAnagrams/groupAnagrams.go
+++ leetcode/solutions/0049.GroupAnagrams/groupAnagrams.go
@@ -1,36 +1,22 @@
 package solution
 
-import (
-	"sort"
-)
-
-type sortRunes []rune
-
-func (s sortRunes) Less(i, j int) bool {
-	return s[i] < s[j]
-}
-
-func (s sortRunes) Len() int {
-	return len(s)
-}
-
-func (s sortRunes) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
 func groupAnagrams(strs []string) [][]string {
-	strMap := make(map[string][]string)
+	const first = 'a'
+	tmp := make(map[[26]int][]string)
 
 	for _, str := range strs {
-		s := []rune(str)
-		sort.Sort(sortRunes(s))
-		key := string(s)
-		strMap[key] = append(strMap[key], str)
+		key := [26]int{}
+
+		for _, r := range str {
+			key[r-first]++
+		}
+
+		tmp[key] = append(tmp[key], str)
 	}
 
-	var result [][]string
+	result := [][]string{}
 
-	for _, strs := range strMap {
+	for _, strs := range tmp {
 		result = append(result, strs)
 	}
 

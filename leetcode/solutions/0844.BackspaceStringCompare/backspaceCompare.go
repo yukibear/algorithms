054fd311db1b5e9118fe70e022c37dec7f71860b
Yuki Sato <yuki.sato.dev@gmail.com>
2020-04-09 10:00:58 -0700

    #0844 refactor

--- leetcode/solutions/0844.BackspaceStringCompare/backspaceCompare.go
+++ leetcode/solutions/0844.BackspaceStringCompare/backspaceCompare.go
@@ -1,21 +1,27 @@
 package solution
 
 func backspaceCompare(S string, T string) bool {
-	return interpritBs(S) == interpritBs(T)
+	si, ti := len(S), len(T)
+
+	for {
+		si, ti = doBackspace(S, si-1), doBackspace(T, ti-1)
+		if si < 0 || ti < 0 || S[si] != T[ti] {
+			return si == -1 && ti == -1
+		}
+	}
 }
 
-func interpritBs(str string) string {
-	res := []rune{}
-
-	for _, s := range str {
-		if s != '#' {
-			res = append(res, s)
-			continue
-		}
-		if len(res) > 0 {
-			res = res[:len(res)-1]
+func doBackspace(s string, i int) int {
+	for bs := 0; i >= 0; i-- {
+		switch {
+		case s[i] == '#':
+			bs++
+		case bs > 0:
+			bs--
+		default:
+			return i
 		}
 	}
 
-	return string(res)
+	return i
 }

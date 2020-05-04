e28b5261e3e8f2b541486cd847109c1c32c513f2
Yuki Sato <yuki.sato.dev@gmail.com>
2020-05-04 03:16:24 -0700

    solve 1-b

---
+++ aizu-online-judge/itp1/1-b/atp1-1-b_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func Test_run(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{"2", "8"},
+		{"8", "512"},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run("Test", func(t *testing.T) {
+			b := &bytes.Buffer{}
+			writer = b
+			reader = strings.NewReader(tt.in)
+			run()
+
+			got := strings.TrimSpace(b.String())
+
+			if got != tt.out {
+				t.Errorf("got %#v want %#v", string(got), tt.out)
+			}
+		})
+	}
+
+}

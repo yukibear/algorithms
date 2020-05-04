49b91335b3d9c518cc6ee97d85952b60c6094c29
Yuki Sato <yuki.sato.dev@gmail.com>
2020-05-04 03:59:36 -0700

    atp1-3-a solve

---
+++ aizu-online-judge/itp1/3-a/atp1-3-a.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	reader io.Reader = os.Stdin
+	writer io.Writer = os.Stdout
+)
+
+func main() {
+	reader = os.Stdin
+	writer = os.Stdout
+	run()
+}
+
+func run() {
+	var result string
+	hello := "Hello World\n"
+
+	for i := 0; i < 1000; i++ {
+		result += hello
+	}
+
+	fmt.Fprint(writer, result)
+}
+
+// readIntsFromStdin parses lines of inputs into 2D int array.
+func readIntsFromStdin() [][]int {
+	out := [][]int{}
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		var ints []int
+
+		for _, s := range strings.Split(scanner.Text(), " ") {
+			i, err := strconv.Atoi(s)
+
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to parse string into int.", err)
+				os.Exit(1)
+			}
+
+			ints = append(ints, i)
+		}
+
+		out = append(out, ints)
+	}
+
+	return out
+}

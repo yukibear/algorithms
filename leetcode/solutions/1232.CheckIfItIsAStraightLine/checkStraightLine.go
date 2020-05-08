d12f5891a1156d208f77491c9692d31db20cf824
Yuki Sato <yuki.sato.dev@gmail.com>
2020-05-08 02:57:14 -0700

    #1232 refactor

--- leetcode/solutions/1232.CheckIfItIsAStraightLine/checkStraightLine.go
+++ leetcode/solutions/1232.CheckIfItIsAStraightLine/checkStraightLine.go
@@ -7,19 +7,13 @@ func checkStraightLine(coordinates [][]int) bool {
 
 	x1, y1 := float64(coordinates[0][0]), float64(coordinates[0][1])
 	x2, y2 := float64(coordinates[1][0]), float64(coordinates[1][1])
-	prevInc := (y2 - y1) / (x2 - x1)
-	x1, y1 = x2, y2
 
 	for i := 2; i < len(coordinates); i++ {
-		x2, y2 = float64(coordinates[i][0]), float64(coordinates[i][1])
+		x3, y3 := float64(coordinates[i][0]), float64(coordinates[i][1])
 
-		inc := (y2 - y1) / (x2 - x1)
-
-		if prevInc != inc {
+		if (y3-y1)/(x3-x1) != (y3-y2)/(x3-x2) {
 			return false
 		}
-
-		x1, y1, prevInc = x2, y2, inc
 	}
 
 	return true

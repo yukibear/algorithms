c485f4dc81c81d28973026b6f03b12386a03f823
Yuki Sato <yuki.sato.dev@gmail.com>
2020-10-04 11:54:28 -0700

    fix(deno): rename function names to follow the latest deno version

--- leetcode/solutions/0771.JewelsAndStones/numJewelsInStones_test.ts
+++ leetcode/solutions/0771.JewelsAndStones/numJewelsInStones_test.ts
@@ -1,7 +1,7 @@
-import { assertStrictEq } from "https://deno.land/std/testing/asserts.ts";
+import { assertStrictEquals } from "https://deno.land/std/testing/asserts.ts";
 import numJewelsInStones from "./numJewelsInStones.ts";
 
 Deno.test("0771. Jewels and Stones", () => {
-  assertStrictEq(numJewelsInStones("aA", "aAAbbbb"), 3);
-  assertStrictEq(numJewelsInStones("z", "ZZ"), 0);
+  assertStrictEquals(numJewelsInStones("aA", "aAAbbbb"), 3);
+  assertStrictEquals(numJewelsInStones("z", "ZZ"), 0);
 });

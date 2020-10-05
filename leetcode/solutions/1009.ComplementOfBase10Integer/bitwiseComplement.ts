4043de9c3d2ee384863582e2e0da623d5396c2b3
Yuki Sato <yuki.sato.dev@gmail.com>
2020-10-05 10:14:23 -0700

    solve by bitwise operation and counting digits

--- leetcode/solutions/1009.ComplementOfBase10Integer/bitwiseComplement.ts
+++ leetcode/solutions/1009.ComplementOfBase10Integer/bitwiseComplement.ts
@@ -1,13 +1,3 @@
 export default function bitwiseComplement(N: number): number {
-  if (N === 0) {
-    return 1;
-  }
-
-  let base2 = "";
-
-  for (; N > 0; N = Math.floor(N / 2)) {
-    base2 = (N % 2 ^ 1).toString() + base2;
-  }
-
-  return parseInt(base2, 2);
+  return N === 0 ? 1 : (Math.pow(2, Math.floor(Math.log2(N)) + 1) - 1) ^ N;
 }
